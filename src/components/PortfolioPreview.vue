f47e247f643423159fe1a060ca8de0c8e77ccd52
nbt53 <nbt853.nh@gmail.com>
2024-05-08 18:54:01 +0100

    portfolio preview updated

---
+++ src/components/PortfolioPreview.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="portfolio__preview">
+    <div class="portfolio__preview-content">
+      <h2 class="mb-medium portfolio__preview-title">{{ document.title }}</h2>
+      <p class="portfolio__preview-description">
+        {{ document.description[0] }}
+      </p>
+      <div class="portfolio__preview-tech">
+        <div v-for="tech in document.technologies" :key="tech.index">
+          <p class="form__list-item mb-small">{{ tech }}</p>
+        </div>
+      </div>
+    </div>
+    <div class="portfolio__preview-imageContainer">
+      <img
+        class="portfolio__preview-image"
+        :src="document.images[0]"
+        alt="Portfolio Image"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    document,
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="error" v-if="error">{{ error }}</div>
   <div v-for="item in portfolios" :key="item.key">
-    <p>{{ item.title }}</p>
+    <PortfolioPreview :document="item" />
   </div>
 </template>
 
 <script>
 import getCollection from "@/composables/getCollection";
+import PortfolioPreview from "@/components/PortfolioPreview.vue";
 export default {
+  components: {
+    PortfolioPreview,
+  },
   setup() {
     const { error, documents: portfolios } = getCollection("portfolio");
     console.log(portfolios);
